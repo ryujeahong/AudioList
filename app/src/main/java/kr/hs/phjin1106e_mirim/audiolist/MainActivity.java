a0e156f50a7a3c32b858fb66a37ff9955a1432c7
ryujeahong <phnin1106@naver.com>
2017-06-19 20:58:29 +0900

    #3 Event Programming

--- app/src/main/java/kr/hs/phjin1106e_mirim/audiolist/MainActivity.java
+++ app/src/main/java/kr/hs/phjin1106e_mirim/audiolist/MainActivity.java
@@ -1,7 +1,10 @@
 package kr.hs.phjin1106e_mirim.audiolist;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -14,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     TextView textMusic;
     ProgressBar progress;
     String[] musics={"music", "music2", "music3"};
+    int[] musicResIds={R.raw.music, R.raw.music2, R.raw.music3};
+    int selectedMusId;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,35 @@ public class MainActivity extends AppCompatActivity {
         list.setAdapter(adapter);
         list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         list.setItemChecked(0, true);
+        selectedMusId=musicResIds[0];
+        mediaPlayer= MediaPlayer.create(this,selectedMusId);
 
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                selectedMusId=musicResIds[i];
+            }
+        });
+
+        butPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.create(MainActivity.this, selectedMusId);
+                mediaPlayer.start();
+            }
+        });
+
+        butStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mediaPlayer.stop();
     }
 }
